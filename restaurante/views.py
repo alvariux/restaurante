3da2829ee57882f4a781b2d1abb50c02e61e4c91
Alvaro Colunga <alvaro.colunga@gmail.com>
2025-10-27 16:22:11 -0500

    se agrego grafica de ventas por semana

--- restaurante/views.py
+++ restaurante/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Sum, Count
 from apps.ordenes.models import Orden
 from datetime import datetime
+from django.db import models
+from datetime import timedelta
 
 def main_index(request):
     return render(request, 'main/index.html')
@@ -14,9 +16,25 @@ def index_user(request):
     total_sales = sum(orden.total for orden in ordenes_hoy)
     cantidad_ordenes = ordenes_hoy.count()
 
+    ordenes_semana = Orden.objects.filter(
+        estatus='pagada', fecha_hora__week=datetime.today().isocalendar()[1]
+    )
+
+    # dias de la semana del 1 (lunes) al 7 (domingo)
+    dias_semana = [datetime.today() - timedelta(days=datetime.today().isocalendar()[2] - 1) + timedelta(days=i) for i in range(7)]
+    ventas_por_dia = []
+    for dia in dias_semana:        
+        ventas_dia = ordenes_semana.filter(fecha_hora__date=dia.date()).aggregate(total=Sum(models.F('detalles__cantidad') * models.F('detalles__precio_unitario')))['total'] or 0
+        ventas_por_dia.append({'dia': dia.strftime('%Y-%m-%d'), 'total': ventas_dia})
+
+
+    print(ventas_por_dia)
+
     context = {
         'ventas_totales': total_sales,
         'cantidad_ordenes': cantidad_ordenes,
+        'ordenes_semana': ordenes_semana,
+        'ventas_por_dia': ventas_por_dia,
     }
 
     return render(request, 'main/main_index.html', context)
